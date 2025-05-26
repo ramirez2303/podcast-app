992d86913e67404084802ee11cfa2013297f5c65
Rodrigo Ramirez <rodrigoramirez@librodepases.com>
2025-05-26 14:39:15 -0300

    implementar persistir los favoritos en localStorage

--- src/stores/useFavoritesStore.ts
+++ src/stores/useFavoritesStore.ts
@@ -1,5 +1,6 @@
 import type { Podcast } from "@/types";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type FavoritesStore = {
     favorites: Record<Podcast["id"], Podcast>;
@@ -10,31 +11,38 @@ type FavoritesStore = {
     getFavoritesList: () => Podcast[];
 };
 
-export const useFavoritesStore = create<FavoritesStore>((set, get) => ({
-    favorites: {},
+export const useFavoritesStore = create<FavoritesStore>()(
+    persist(
+        (set, get) => ({
+            favorites: {},
 
-    addPodcast: (podcast) =>
-        set((state) => ({
-            favorites: { ...state.favorites, [podcast.id]: podcast },
-        })),
+            addPodcast: (podcast) =>
+                set((state) => ({
+                    favorites: { ...state.favorites, [podcast.id]: podcast },
+                })),
 
-    removePodcast: (podcastId) =>
-        set((state) => {
-            const updated = { ...state.favorites };
-            delete updated[podcastId];
-            return { favorites: updated };
+            removePodcast: (podcastId) =>
+                set((state) => {
+                    const updated = { ...state.favorites };
+                    delete updated[podcastId];
+                    return { favorites: updated };
+                }),
+
+            isFavorite: (podcastId) => !!get().favorites[podcastId],
+
+            handleFavorite: (podcast) => {
+                const isFavorite = get().isFavorite(podcast.id);
+                if (isFavorite) {
+                    get().removePodcast(podcast.id);
+                } else {
+                    get().addPodcast(podcast);
+                }
+            },
+
+            getFavoritesList: () => Object.values(get().favorites),
         }),
-
-    isFavorite: (podcastId) => !!get().favorites[podcastId],
-
-    handleFavorite: (podcast) => {
-        const isFavorite = get().isFavorite(podcast.id);
-        if (isFavorite) {
-            get().removePodcast(podcast.id);
-        } else {
-            get().addPodcast(podcast);
+        {
+            name: "favorites-storage",
         }
-    },
-
-    getFavoritesList: () => Object.values(get().favorites),
-}));
+    )
+);
