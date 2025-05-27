882d5bca61cf0a45597077164468135f275245e6
Rodrigo Ramirez <rodrigoramirez@librodepases.com>
2025-05-26 23:09:40 -0300

    ajustar apikey

--- src/components/common/EmptyState/EmptyState.test.tsx
+++ src/components/common/EmptyState/EmptyState.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from '@testing-library/react';
 import EmptyState from "./EmptyState";
 
 describe("EmptyState", () => {
